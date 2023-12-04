5dabbadd1197fa4f8067e07ecf4ff8ce7875e552
johannghub <90212860+jtgithub16@users.noreply.github.com>
2023-12-04 07:39:51 -0500

    Add files via upload

---
+++ javascript/scripts.js
@@ -0,0 +1,12 @@
+function redirectLinkedin(){
+    window.location.href = 'https://www.linkedin.com/in/johann-tsegay-657457210/';
+
+}
+function redirectGIT(){
+    window.location.href = 'https://github.com/jtgithub16';
+
+}
+function redirectVSCO(){
+    window.location.href = 'https://gemma.vsco.co/johanntse/gallery';
+
+}
